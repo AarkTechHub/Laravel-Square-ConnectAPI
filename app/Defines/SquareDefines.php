e5e4d18e5c0281a6653de45d9e0a90d947ff1ffe
DevilAngel <pranay.aark@gmail.com>
2021-08-18 15:07:13 +0530

    added defines

--- app/Defines/SquareDefines.php
+++ app/Defines/SquareDefines.php
@@ -5,9 +5,9 @@ class SquareDefines
 {
     //SquareDefines
     public const ENVIRONMENT = "sandbox";
-    public const SQUARE_APPLICATION_ID = "sandbox-sq0idb-cbVk_DFGPtVPv9h24pQChQ";
-    public const SQUARE_ACCESS_TOKEN = "EAAAEBp2-9iT5ql6YBuxr_ocYImklC915glKsSnIklc8zFo69E36xNL8a31HBowl";
-    public const SQUARE_LOCATION_ID = "LG5WE68CJSXVZ";
+    public const SQUARE_APPLICATION_ID = "Your-sandbox-ID";
+    public const SQUARE_ACCESS_TOKEN = "Your-access-token";
+    public const SQUARE_LOCATION_ID = "Your-location-id";
     public const WEB_PAYMENT_SDK_URL = "https://sandbox.web.squarecdn.com/v1/square.js";
 }
 
